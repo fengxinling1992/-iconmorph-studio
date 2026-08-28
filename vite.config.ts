1f66d0d8554e1ad73c30fef8172cebae83eed4ec
fengxinling1992 <47844206+fengxinling1992@users.noreply.github.com>
2026-08-28 15:39:56 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -206,7 +206,7 @@ function vitePluginStorageProxy(): Plugin {
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(), vitePluginManusDebugCollector(), vitePluginStorageProxy()];
 
 export default defineConfig({ 
-  base: '/iconmorph-studio/',
+base: '/-iconmorph-studio/',
   plugins,
   resolve: {
     alias: {
